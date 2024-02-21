c3301354efff37d6f9273d5502ede246931f66a3
Kreusna Bayu <kreusna.bayu@mhs.itenas.ac.id>
2024-02-21 08:24:10 +0700

    Update quiz_controller.dart

--- tryout/lib/controller/quiz_controller.dart
+++ tryout/lib/controller/quiz_controller.dart
@@ -17,47 +17,48 @@ class QuizController {
     }
   }
 
-  Future<void> fetchQuestions() async {
-    try {
-      final List<Map<String, dynamic>> questionsData = await ApiSoal.fetchQuestions();
+Future<void> fetchQuestions() async {
+  try {
+    final List<Map<String, dynamic>> questionsData = await ApiSoal.fetchQuestions();
 
-      if (questionsData.isNotEmpty) {
-        for (var data in questionsData) {
-          if (data.containsKey('soal') && data.containsKey('tryout_question_option')) {
-            final List<Map<String, dynamic>> options =
-                List<Map<String, dynamic>>.from(data['tryout_question_option']);
+    if (questionsData.isNotEmpty) {
+      for (var data in questionsData) {
+        if (data.containsKey('soal') && data.containsKey('tryout_question_option')) {
+          final List<Map<String, dynamic>> options =
+              List<Map<String, dynamic>>.from(data['tryout_question_option']);
 
-            final int tryoutQuestionId = int.parse(data['tryout_question_id']);
+          final int tryoutQuestionId = int.parse(data['tryout_question_id']);
+          final int questionId = int.parse(data['id']); // Tambahkan baris ini
 
-            _questions.add(Question(
-              id: data['id'],
-              questionText: data['soal'],
-              options: options.map((option) {
-                return Option(
-                  id: option['id'],
-                  tryoutQuestionId: option['tryout_question_id'],
-                  inisial: option['inisial'],
-                  jawaban: option['jawaban'],
-                  isCorrect: option['iscorrect'] == '1',
-                  nilai: int.parse(option['nilai']),
-                );
-              }).toList(),
-              tryoutQuestionId: tryoutQuestionId,
-            ));
-          } else {
-            print('Error: Invalid data format in API response');
-            throw Exception('Failed to load question: Invalid data format');
-          }
+          _questions.add(Question(
+            id: questionId, // Ubah ini
+            questionText: data['soal'],
+            options: options.map((option) {
+              return Option(
+                id: option['id'],
+                tryoutQuestionId: option['tryout_question_id'],
+                inisial: option['inisial'],
+                jawaban: option['jawaban'],
+                isCorrect: option['iscorrect'] == '1',
+                nilai: int.parse(option['nilai']),
+              );
+            }).toList(),
+            tryoutQuestionId: tryoutQuestionId,
+          ));
+        } else {
+          print('Error: Invalid data format in API response');
+          throw Exception('Failed to load question: Invalid data format');
         }
-      } else {
-        print('Error: No questions available');
-        throw Exception('Failed to load questions: No questions available');
       }
-    } catch (error) {
-      print('Error fetching questions: $error');
-      throw Exception('Failed to load questions: $error');
+    } else {
+      print('Error: No questions available');
+      throw Exception('Failed to load questions: No questions available');
     }
+  } catch (error) {
+    print('Error fetching questions: $error');
+    throw Exception('Failed to load questions: $error');
   }
+}
 
   void nextQuestion() {
     if (_currentIndex < _questions.length - 1) {
